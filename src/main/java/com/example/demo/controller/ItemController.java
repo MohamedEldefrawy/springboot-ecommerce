036bd17155ffd4ca411f4ab88f88e048076edf6a
MohamedEldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-12-11 21:49:36 +0200

    Add logs in Cart,Item and Order controllers

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,32 +11,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Item;
-import com.example.demo.model.persistence.repositories.ItemRepository;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/item")
 public class ItemController {
 
-	@Autowired
-	private ItemRepository itemRepository;
-	
-	@GetMapping
-	public ResponseEntity<List<Item>> getItems() {
-		return ResponseEntity.ok(itemRepository.findAll());
-	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
-		return ResponseEntity.of(itemRepository.findById(id));
-	}
-	
-	@GetMapping("/name/{name}")
-	public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
-		List<Item> items = itemRepository.findByName(name);
-		return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
-				: ResponseEntity.ok(items);
-			
-	}
-	
+    @Autowired
+    private ItemRepository itemRepository;
+    private final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getItems() {
+        List<Item> items = itemRepository.findAll();
+        if (items.size() == 0)
+            logger.warn("No Items found");
+        return ResponseEntity.ok(itemRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        Optional<Item> itemOptional = itemRepository.findById(id);
+        if (!itemOptional.isPresent())
+            logger.warn("No item found with id:" + itemOptional);
+        return ResponseEntity.of(itemOptional);
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+        List<Item> items = itemRepository.findByName(name);
+        if (items == null || items.isEmpty()) {
+            logger.warn("No Item found with name: " + name);
+            return ResponseEntity.notFound().build();
+        } else
+            return ResponseEntity.ok(items);
+    }
+
 }
